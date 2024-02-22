e4d59b145e31e2d0a36288e2afabc49e0d7897e7
vavogmlfkr3@gmail.com <vavogmlfkr3@gmail.com>
2024-02-22 15:49:47 +0900

    업데이트

--- quiz/src/main/java/com/example/demo/RestController/MyRestController.java
+++ quiz/src/main/java/com/example/demo/RestController/MyRestController.java
@@ -7,6 +7,8 @@ import java.net.URL;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyRestController {
 	
 	@GetMapping("pages")
-	public String getQuiz(@RequestParam(name="url") String url, @RequestParam(name="token") String token) throws IOException {
+	public String getPages(@RequestParam(name="url") String url, @RequestParam(name="token") String token) throws IOException {
 		URL userMainPage = new URL(url); 
 		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(userMainPage.openStream(),"UTF-8"));
@@ -28,9 +30,17 @@ public class MyRestController {
 		}
 		
 		bf.close();
-		Document dom = Jsoup.parse(String.valueOf(html));
+		Document dom = Jsoup.parse(html.toString());
 		
+		Elements pageList = dom.select(".entries-list");
 		
+		StringBuilder pageJson = new StringBuilder("[");
+		for(Element e : pageList) {
+			StringBuilder json = new StringBuilder("{");
+				
+			json.append("}");
+		}
+		pageJson.append("]");
 		
 		return null;
 	}
