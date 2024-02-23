a32eba86a4dc75f91a71a6dd7fc01cad2eb79189
vavogmlfkr3@gmail.com <vavogmlfkr3@gmail.com>
2024-02-23 12:16:07 +0900

    업데이트

--- quiz/src/main/java/com/example/demo/restController/MyRestController.java
+++ quiz/src/main/java/com/example/demo/restController/MyRestController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import rest.PageVO;
-
 
 @RestController
 public class MyRestController {
@@ -23,16 +21,25 @@ public class MyRestController {
 	@GetMapping("pages")
 	public List<Map<String,Object>> getPages(@RequestParam(name="url") String url, @RequestParam(name="token") String token) throws IOException {
 		Document dom = Jsoup.connect(url).get();
-		
+		url = url.substring(0,url.lastIndexOf(".io")+3);
 		Elements pageList = dom.select(".page__content.e-content"); 
 		
 		List<Map<String,Object>> returnList = new ArrayList<>();
  
 		Elements pageChild = pageList.get(0).children();
 		for(int i = 0; i < pageChild.size(); i++) {
-			//pd.setUrl(e.select("a").attr("href"));
 			Map<String,Object> pageChildData = new HashMap<String, Object>();
+			
+			Document childDom = Jsoup.connect(url+pageChild.get(i).select("a").attr("href")).get();
+			Elements mainDom = childDom.select(".page__content.e-content > ul");
+			
+			Elements tables = mainDom.get(0).children();
+			List<String> childs = new ArrayList<String>();
+			for(Element e : tables) {
+				childs.add(e.select("h1").text());
+			}
 			pageChildData.put("title", pageChild.get(i).select("a").text());
+			pageChildData.put("childs", childs);
 			returnList.add(pageChildData);
 		}
 		
