aa3cd4891eaad60a24f5553379961a9d6cf43a3f
vavogmlfkr3@gmail.com <vavogmlfkr3@gmail.com>
2024-02-26 14:58:13 +0900

    업데이트

--- quiz/src/main/java/com/example/demo/main/MainController.java
+++ quiz/src/main/java/com/example/demo/main/MainController.java
@@ -1,16 +1,9 @@
 package com.example.demo.main;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,88 +37,9 @@ public class MainController {
 
 	@RequestMapping(value="quiz", method=RequestMethod.POST)
 	@ResponseBody
-	@SuppressWarnings("unchecked")
 	public List<Map<String,Object>> getQuize(@RequestBody Map<String,Object> body) throws IOException {
 		
-		int langCnt = 2; 
-		int typeCnt = 2;
-		
-	
-		
-		List<Map<String,Object>> json = (List<Map<String,Object>>)body.get("json");
-		int cnt = (int)body.get("cnt");
-		int type = (int)body.get("type");
-		String url = (String)body.get("url");
-		
-		int allQuizCnt = json.size()*langCnt*typeCnt;
-		allQuizCnt = allQuizCnt < cnt ? allQuizCnt : cnt;
-		
-		
-		Document dom = Jsoup.connect(url).get();
-		url = url.substring(0,url.indexOf(".io")+3);
-		Elements pageList = dom.select(".page__content.e-content"); 
-		
-		
-		Map<String,Object> quizs = new HashMap<>();  
-		
-		Map<Integer,Integer> ranQuiz = new HashMap<Integer,Integer>();
-		List<Integer> ranArr = new ArrayList<Integer>();
-		List<Integer> keys = new ArrayList<Integer>();
-		
-		for(int i = 0; i < json.size(); i++) {
-			ranArr.add(i);
-		}
-			
-		for(int i = 0; i < allQuizCnt; i++) {
-			int index = mainService.RAN(0, ranArr.size()-1); //ranArr의 무작위 인덱스값
-			int ran = ranArr.get(index); //raArr의 해당 인덱스의 값
-			
-			if(ranQuiz.get(ran) == null) {
-				ranQuiz.put(ran, 1);
-				keys.add(ran);
-			}
-			else if(ranQuiz.get(ran) != langCnt*typeCnt) {
-				ranQuiz.compute(ran, (k,v) -> v= v+1);
-				if(ranQuiz.get(ran) == langCnt*typeCnt)
-					ranArr.remove(index);
-			}
-		}
-		
-		
-		
-		
-		
-		for(int i = 0; i < allQuizCnt; i++) {
-			int key = mainService.RAN(0, keys.size()-1);
-			int index = keys.get(key);
-			
-			//index하나 가져와서 빼는 작업
-			ranQuiz.compute(index, (k,v) -> v= v-1);
-			if(ranQuiz.get(index) == 0) {
-				ranQuiz.remove(index);
-				keys.remove(key);
-			}
-			System.out.println(json.get(index));
-			
-			switch(type) {
-			case 1: //객관식
-				break;
-			case 2: //주관식
-				
-				break;
-			case 3: // 혼합
-				
-				
-				
-				
-				
-				break;
-			default:
-				break;
-			}
-		}
-		
-		
+		mainService.getQuize(body);
 		return null;
 	}
 	
--- quiz/src/main/java/com/example/demo/main/MainService.java
+++ quiz/src/main/java/com/example/demo/main/MainService.java
@@ -6,15 +6,32 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class MainService {
+	private int langCnt = 2; 
+	private int typeCnt = 2;
+	
+	private Map<String,Object> settingFunc = new HashMap<String, Object>();
+	
+	public MainService() {
+		Function<Integer, Map<String,Object>> f1 = s -> quiz1(s);
+		
+		settingFunc.put("func", f1);
+		settingFunc.put("func", f1);
+		System.out.println(settingFunc.get("func").getClass()); 
+	}
+	
+	
+	
 	
 	public List<Map<String,Object>> getPages(String url, String token) throws IOException {
 		if(!url.contains("http"))return null;
@@ -73,13 +90,118 @@ public class MainService {
 				Map<String,Object> item = new HashMap<String, Object>();
 				
 				item.put("en", e.children().get(0).text());
-				item.put("kr", e.children().get(0).text());
+				item.put("kr", e.children().get(1).text());
 				
 				quizjson.add(item);
 			}
 		}
 		return quizjson;
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Map<String,Object>> getQuize(@RequestBody Map<String,Object> body) throws IOException {
+		
+		List<Map<String,Object>> json = (List<Map<String,Object>>)body.get("json");
+		int cnt = (int)body.get("cnt");
+		int type = (int)body.get("type");
+		String url = (String)body.get("url");
+		
+		int allQuizCnt = json.size()*langCnt*typeCnt;
+		allQuizCnt = allQuizCnt < cnt ? allQuizCnt : cnt;
+		
+		
+		
+		
+		Map<String,Object> quizs = new HashMap<>();  
+		
+		Map<Integer,Integer> indexCnt = new HashMap<Integer,Integer>();
+		List<Integer> keys = new ArrayList<Integer>();
+		
+		addRandomQuiz(json.size(), allQuizCnt, indexCnt, keys);
+		System.out.println(indexCnt);
+		
+		
+		
+		
+		
+		Map<Integer,List<Integer>> indexTitle = new HashMap<Integer, List<Integer>>();
+		
+		for(int i = 0; i < keys.size(); i++) {
+			int key = keys.get(i);
+			List<Integer> su = new ArrayList<Integer>();
+			for(int j = 0; j < langCnt*typeCnt; j++) {
+				su.add(j);
+			}
+			indexTitle.put(key, su);
+		}
+		
+		
+		
+		
+		for(int i = 0; i < allQuizCnt; i++) {
+			int index = getRandomQuiz(indexCnt, keys);
+			Map<String,Object> oneJson = json.get(index);
+			
+			
+			
+		
+		}
+		
+		
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	//랜덤으로 선택하는 함수 한문제 당 최대 언어수 * 문제 형태로 가질수 있다
+	public void addRandomQuiz(int jsonSize, int allQuizCnt, Map<Integer,Integer> indexCnt, List<Integer> keys) {
+		List<Integer> ranArr = new ArrayList<Integer>();
+		
+		for(int i = 0; i < jsonSize; i++) {
+			ranArr.add(i);
+		}
+			
+		for(int i = 0; i < allQuizCnt; i++) {
+			int index = RAN(0, ranArr.size()-1); //ranArr의 무작위 인덱스값
+			int ran = ranArr.get(index); //raArr의 해당 인덱스의 값
+			
+			if(indexCnt.get(ran) == null) {
+				indexCnt.put(ran, 1);
+				keys.add(ran);
+			}
+			else if(indexCnt.get(ran) != langCnt*typeCnt) {
+				indexCnt.compute(ran, (k,v) -> v= v+1);
+				if(indexCnt.get(ran) == langCnt*typeCnt)
+					ranArr.remove(index);
+			}
+		}
+	}
+	
+	public int getRandomQuiz(Map<Integer,Integer> indexCnt, List<Integer> keys) {
+		int key = RAN(0, keys.size()-1);
+		int index = keys.get(key);
+		
+		//index하나 가져와서 빼는 작업
+		indexCnt.compute(index, (k,v) -> v= v-1);
+		if(indexCnt.get(index) == 0) {
+			indexCnt.remove(index);
+			keys.remove(key);
+		}
+		return index;
+	}
+	
+	
+	public Map<String,Object> quiz1(int a){
+		return null;
+	}
+	
+	
+	
+	
+	
 	
 	
 	
